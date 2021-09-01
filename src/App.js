923bda0909f4b7763f0fa42ed519a4ccb75aeffd
lixiaolong <xiaolong.li@shopee.com>
2021-09-01 22:14:04 +0800

    feat: 灰度版本3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>灰度版本2</h1>
+        <h1>灰度版本3</h1>
       </header>
     </div>
   );
