a022ce9361f45a4e03d02097a6fad2039016fc8a
lixiaolong <xiaolong.li@shopee.com>
2021-09-01 22:14:53 +0800

    feat: 正式版本

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>正式版本2</h1>
+        <h1>正式版本</h1>
       </header>
     </div>
   );
